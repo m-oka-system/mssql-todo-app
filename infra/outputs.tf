074e7d144e06193a1e9f09f77e9ca6a81f0c7a0d
m-oka-system <m-oka-system@outlook.com>
2026-07-29 17:03:48 +0900

    refactor: ssh_command に含まれる ssh_private_key_path の出力を削除

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,7 +1,3 @@
-output "ssh_private_key_path" {
-  value = local_sensitive_file.ssh_private_key.filename
-}
-
 output "ssh_command" {
   value = "ssh -i ${local_sensitive_file.ssh_private_key.filename} ${module.vm.admin_username}@${module.vm.vm_public_ip["vm01"].ip_address}"
 }
