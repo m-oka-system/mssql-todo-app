0b5ae9386ddbe3a1aeaed831dbbe90d30a6ed3ef
m-oka-system <m-oka-system@outlook.com>
2026-08-02 19:18:51 +0900

    chore: Terraform プロバイダのバージョンを完全一致で固定する

--- infra/terraform.tf
+++ infra/terraform.tf
@@ -2,29 +2,29 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.81.0"
+      version = "=4.81.0"
     }
     tls = {
       source  = "hashicorp/tls"
-      version = "~> 4.3.0"
+      version = "=4.3.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.9.0"
+      version = "=3.9.0"
     }
     local = {
       source  = "hashicorp/local"
-      version = "~> 2.9.0"
+      version = "=2.9.0"
     }
     # 無料オファー（useFreeLimit）は azurerm が未対応のため azapi を併用する
     azapi = {
       source  = "Azure/azapi"
-      version = "~> 2.11.0"
+      version = "=2.11.0"
     }
     # ファイアウォール規則へ実行元の IP を自動で登録するために使う
     http = {
       source  = "hashicorp/http"
-      version = "~> 3.6.0"
+      version = "=3.6.0"
     }
   }
 }
