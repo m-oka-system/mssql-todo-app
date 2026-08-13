9d172aa3eb45677c7a739acd00098d7a91765772
m-oka-system <m-oka-system@outlook.com>
2026-08-13 14:12:39 +0900

    docs: allowed_client_ip の指定方法を説明するコメントを削除する

--- labs/sections/section09/variables.tf
+++ labs/sections/section09/variables.tf
@@ -7,10 +7,6 @@ variable "location" {
   default = "japaneast"
 }
 
-# SSH（22 番）と SQL のファイアウォールで許可する送信元。HTTP は Internet へ公開するため、ここに含める必要はない
-# terraform apply を実行した環境の IP は自動で許可されるため、それ以外に許可したい IP だけをここに指定する
-#   Cloud Shell で apply し、ローカルからも接続する場合 : ローカルの IP を指定する
-#   ローカルだけで進める場合                            : 指定は不要
 variable "allowed_client_ip" {
   type    = list(string)
   default = []
